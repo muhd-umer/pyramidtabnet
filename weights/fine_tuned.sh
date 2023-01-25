e2623b8629d86ec7e356f6b1f9e342408d2c0fb2
Muhammad Umer <thisismumer@gmail.com>
2023-01-26 00:24:52 +0500

    Rename .pth

--- weights/fine_tuned.sh
+++ weights/fine_tuned.sh
@@ -3,6 +3,5 @@
 
 wget -O weights/marmot.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.0/marmot.pth
 wget -O weights/icdar2017.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.0/icdar2017.pth
-wget -O weights/icdar2019.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.0/icdar2019.pth
 wget -O weights/tablebank.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.0/tablebank.pth
 wget -O weights/unlv.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.0/unlv.pth

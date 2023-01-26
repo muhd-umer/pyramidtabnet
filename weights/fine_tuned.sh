e90323966a113048feeaf2214febeb2e6dfec784
Muhammad Umer <thisismumer@gmail.com>
2023-01-26 22:42:26 +0500

    Updated weight links

--- weights/fine_tuned.sh
+++ weights/fine_tuned.sh
@@ -1,7 +1,7 @@
 # Download all fine-tuned weights from the release 
 # and place them in the weights directory
 
-wget -O weights/marmot.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/marmot.pth
-wget -O weights/icdar2017.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/icdar2017.pth
-wget -O weights/tablebank.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/tablebank.pth
-wget -O weights/unlv.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/unlv.pth
+wget -O weights/marmot.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/marmot.pth
+wget -O weights/icdar2017.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/icdar2017.pth
+wget -O weights/tablebank.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/tablebank.pth
+wget -O weights/unlv.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/unlv.pth
--- weights/get_weights.sh
+++ weights/get_weights.sh
@@ -1,9 +1,9 @@
 # Download the base and best weights from the release 
 # and place them in the weights directory
 
-wget -O weights/base.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/base.pth
-wget -O weights/ptn_detection.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/ptn_detection.pth
-wget -O weights/ptn_recognition.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/ptn_recognition.pth
-wget -O weights/ptn_cells.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/ptn_cells.pth
-wget -O weights/craft_refiner.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/craft_refiner.pth
-wget -O weights/craft_mlt.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v0.1.1/craft_mlt.pth
+wget -O weights/base.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/base.pth
+wget -O weights/ptn_detection.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/ptn_detection.pth
+wget -O weights/ptn_recognition.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/ptn_recognition.pth
+wget -O weights/ptn_cells.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/ptn_cells.pth
+wget -O weights/craft_refiner.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/craft_refiner.pth
+wget -O weights/craft_mlt.pth https://github.com/muhd-umer/pyramidtabnet/releases/download/v1.0.0/craft_mlt.pth
